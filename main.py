746c0c95320e2a122e36f7d0a0146f8936413d22
SANECHEK114 <ermolaevsasa114@gmail.com>
2025-11-13 01:25:37 +0300

    Почти все готово

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-from PyQt6.QtWidgets import (QApplication, QStackedWidget, QWidget,
+from PyQt6.QtWidgets import (QApplication,
+                             QInputDialog, QMessageBox,
+                             QTableWidgetItem, QHeaderView,
                              QMainWindow)
 import sys
-from table_search import Ui_form as TS_ui
-from addAnimal import Ui_Form as AA_ui
+import sqlite3
+from table_search import Table_Search_CLASS
+from addAnimal import AddAnimal_CLASS
+from sql_querys import (create_db_query, search_query,
+                        delete_query, create_row_query)
 
 
-class Table_Search_CLASS(QWidget, TS_ui):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-
-
-class AddAnimal_CLASS(QWidget, AA_ui):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-
-
-class Window(QMainWindow):
+class Window(QMainWindow, ):
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -25,26 +18,79 @@ class Window(QMainWindow):
     def initUI(self):
         self.setWindowTitle('Учет сельскохозяйственых животных')
         self.setGeometry(100, 100, 1080, 720)
+        with open('config.txt', mode='r', encoding='utf-8') as f:
+            self.settings = f.read().split('\n')
+        self.con = sqlite3.connect(self.settings[0])
+        self.cur = self.con.cursor()
+        self.modified = {}
+        self.columnsinbd = {0: 'id',
+                            1: 'name = "{}"',
+                            2: 'type = "{}"',
+                            3: 'day = {}',
+                            4: 'month = {}',
+                            5: 'year = {}',
+                            6: 'breed = "{}"',
+                            7: 'code = "{}"'}
         # создание менюшки
         menu_bar = self.menuBar()
-        menu = menu_bar.addMenu('файл')
-        open_act = menu.addAction('открыть файл')
-        rename_act = menu.addAction('переименовать файл')
-        delete_act = menu.addAction('удалить файл')
+        menu = menu_bar.addMenu('Таблица')
+        open_act = menu.addAction('Выбрать другую таблицу')
+        rename_act = menu.addAction('переименовать таблицу')
+        create_act = menu.addAction('создать таблицу')
+        delete_act = menu.addAction('удалить Таблицу')
         # подлючение меню к функциям
         open_act.triggered.connect(self.open_file)
         rename_act.triggered.connect(self.rename_file)
+        create_act.triggered.connect(self.create_file)
         delete_act.triggered.connect(self.delete_file)
-        table_searchWidget = Table_Search_CLASS()
-        addAnimalWidget = AddAnimal_CLASS()
-        self.layout = QStackedWidget()
-        self.layout.addWidget(table_searchWidget)
-        self.layout.addWidget(addAnimalWidget)
-        self.setCentralWidget(self.layout)
+        self.table_searchWidget = Table_Search_CLASS()
+        self.addAnimalWidget = AddAnimal_CLASS()
+        self.setCentralWidget(self.table_searchWidget)
         # подключение кнопки
-        table_searchWidget.add_btn.clicked.connect(self.add_animal)
+        self.table_searchWidget.add_btn.clicked.\
+            connect(lambda: self.addAnimalWidget.show())
+        self.addAnimalWidget.save_btn.clicked.connect(self.save)
+        self.table_searchWidget.del_btn.clicked.connect(self.delete_animal)
+        # подключение поисковой строки
+        self.table_searchWidget.search_line.textChanged.\
+            connect(self.load_table)
+        # таблица
+        self.table_searchWidget.tableWidget.setColumnCount(6)
+        self.table_searchWidget.tableWidget.setRowCount(0)
+        header = ['id', 'Имя', 'Вид', 'Дата рождения',
+                  'Порода', 'Код на ушной бирке']
+        self.table_searchWidget.tableWidget.setHorizontalHeaderLabels(header)
+        header = self.table_searchWidget.tableWidget.horizontalHeader()
+        header.setSectionResizeMode(0, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(1, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(2, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(3, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(4, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(5, QHeaderView.ResizeMode.ResizeToContents)
+        # self.table_searchWidget.tableWidget.itemChanged.\
+        #     connect(self.save_changes)
+        self.load_table()
 
-        addAnimalWidget.save_btn.clicked.connect(self.save)
+    def load_table(self):
+        s = self.table_searchWidget.search_line.text()
+        res = self.cur.execute(search_query(s)).fetchall()
+        self.table_searchWidget.tableWidget.setRowCount(0)
+        for i, row in enumerate(res):
+            self.table_searchWidget.tableWidget.setRowCount(
+                self.table_searchWidget.tableWidget.rowCount() + 1)
+            self.table_searchWidget.tableWidget.setItem(
+                i, 0, QTableWidgetItem(str(row[0])))
+            self.table_searchWidget.tableWidget.setItem(
+                i, 1, QTableWidgetItem(str(row[1])))
+            self.table_searchWidget.tableWidget.setItem(
+                i, 2, QTableWidgetItem(str(row[2])))
+            self.table_searchWidget.tableWidget.setItem(
+                i, 3, QTableWidgetItem(str(f"{row[3]}.{row[4]}.{row[5]}")))
+            self.table_searchWidget.tableWidget.setItem(
+                i, 4, QTableWidgetItem(str(row[6])))
+            self.table_searchWidget.tableWidget.setItem(
+                i, 5, QTableWidgetItem(str(row[7])))
+        self.modified = {}
 
     def open_file(self):
         pass
@@ -55,14 +101,56 @@ class Window(QMainWindow):
     def delete_file(self):
         pass
 
-    def add_animal(self):
-        self.layout.setCurrentIndex(1)
-
-    def edit_pet(self):
+    def create_file(self):
         pass
 
+    def delete_animal(self):
+        row = self.table_searchWidget.tableWidget.currentRow()
+        if row != -1:
+            name = self.table_searchWidget.tableWidget.item(row,
+                                                            1).text().strip()
+            id = self.table_searchWidget.tableWidget.item(row, 0).text()
+            reply = QMessageBox.question(
+                self,
+                "Вопрос",
+                f"Вы уверены, что хотите удалить\
+                      {name} ?",
+                QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+                QMessageBox.StandardButton.No)
+            print(id)
+            if reply == QMessageBox.StandardButton.Yes:
+                self.cur.execute(delete_query(id))
+                self.con.commit()
+                self.load_table()
+                print('delete')
+#
+#    def save_changes(self, item):
+#        id = self.table_searchWidget.tableWidget.item(item.row(, 0).text()
+#        self.modified[id] = item.text()
+#        print(item.row(), item.text())
+
     def save(self):
-        self.layout.setCurrentIndex(0)
+        name = self.addAnimalWidget.name.text()
+        breed = self.addAnimalWidget.breed.text()
+        type = self.addAnimalWidget.species.currentText()
+        year, month, day = self.addAnimalWidget.DateEdit.date().getDate()
+        code = self.addAnimalWidget.code.text()
+        print(name, type, day, month, year, breed, code)
+        if all([name, breed, code]):
+            self.cur.execute(create_row_query(name, type, day,
+                                              month, year, breed, code))
+            self.addAnimalWidget.close()
+            self.con.commit()
+            self.load_table()
+        else:
+            QMessageBox.information(
+                self,
+                "Скотина",
+                "Все поля должны быть заполнены.",
+                buttons=QMessageBox.StandardButton.Ok)
+
+    def closeEvent(self, event):
+        self.con.close()
 
 
 if __name__ == "__main__":
---
+++ sql_querys.py
@@ -0,0 +1,31 @@
+create_db_query = '''CREATE TABLE animals (
+    id    INTEGER PRIMARY KEY AUTOINCREMENT
+                  NOT NULL,
+    name  TEXT    NOT NULL,
+    type  TEXT    NOT NULL,
+    day   INTEGER NOT NULL,
+    month INTEGER NOT NULL,
+    year  INTEGER NOT NULL,
+    breed TEXT    NOT NULL,
+    code  TEXT
+);'''
+
+
+def search_query(s):
+    return f'''SELECT * FROM animals
+    WHERE name LIKE '%{s}%' OR type LIKE '%{s}%' OR
+        code LIKE "%{s}%" OR breed LIKE "%{s}%"
+    ORDER BY name'''
+
+
+def delete_query(s):
+    return f'DELETE FROM animals WHERE id = {s}'
+
+
+def create_row_query(name, type, day, month, year, breed, code=''):
+    return f"INSERT INTO animals(name, type, day, month, year, breed, code) \
+        VALUES('{name}', '{type}', {day}, \
+            {month}, {year}, '{breed}', '{code}')"
+
+
+'%{self.search}%'
